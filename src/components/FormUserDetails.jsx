4b54513a66aa7485a524a880b71f868b702a7a4a
mhmtkarakas <m.karakas32@gmail.com>
2024-07-05 18:35:57 +0300

    FormUserDetails

--- src/components/FormUserDetails.jsx
+++ src/components/FormUserDetails.jsx
@@ -1,7 +1,22 @@
 import React from 'react'
-import { ThemeProvider } from '@mui/material/styles';
+import Dialog from "@mui/material/Dialog";
+import  AppBar  from '@mui/material/AppBar';
+import  TextField  from '@mui/material/TextField';
+import  Button  from '@mui/material/Button';
+import  Toolbar  from '@mui/material/Toolbar';
+import  Typography  from '@mui/material/Typography';
+import {createTheme, ThemeProvider } from '@mui/material/styles';
 
-const FormUserDetails = () => {
+const theme = createTheme();
+
+
+
+const FormUserDetails = ({nextStep,values,handeleChange}) => {
+  const continueHandler = (e) => {
+    e.preventDefault();
+    nextStep();
+  
+  }
   return (
     <div>FormUserDetails</div>
   )
